acfbe5e6de3733b759453199ab090b8e08079cfb
Mrsazza <shahriarhossain147@gmail.com>
2021-12-24 23:31:15 +0600

    Initial Commit

---
+++ Football Star Rate/Football_Star_RateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_Star_RateApp.swift
+//  Football Star Rate
+//
+//  Created by Sazza on 24/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Football_Star_RateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
